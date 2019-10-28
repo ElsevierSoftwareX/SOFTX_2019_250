dbc11bd597ed79cf0583f5c6b6f58c0b6502bce8
Daniel Silvestre <50275889+danielmsilvestre@users.noreply.github.com>
2019-10-28 01:23:51 +0000

    v1.2 - corrected license text header

--- src/Examples/Desync/Desync.m
+++ src/Examples/Desync/Desync.m
@@ -1,8 +1,25 @@
 clearvars;
 close all;
 
+% Copyright 2019 Daniel Silvestre
+% This file is part of OPTool.
+%
+% OPTool is free software: you can redistribute it and/or modify 
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+% 
+% OPTool is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with OPTool.  If not, see <https://www.gnu.org/licenses/>.
+
+
 % Number of nodes in the network
-n = 200;
+n = 5;
 
 % Matrix D and vectors defining the g(x) = 0.5||Dx-v 1_n+e_n||^2
 D = -eye(n) + diag(ones(1,n-1),1) + e(n,n)*e(1,n)';
@@ -12,7 +29,8 @@ v = 1/n;
 en = e(n,n);
 
 % Number of maximum iterations for the simulation
-max_iterations = round(n/10)*50;
+% max_iterations = round(n/10)*50;
+max_iterations = 20;
 
 % Define gradient function
 grad = @(x) (D'*D*x+D'*en);
